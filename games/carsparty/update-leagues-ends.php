c62639ad8a7d3aba1784155e9dfc4b201129fa57
Sajad Beigjani <sajad.b@gmail.com>
2022-08-17 04:40:38 +0430

    finalized

--- games/carsparty/update-leagues-ends.php
+++ games/carsparty/update-leagues-ends.php
@@ -1,40 +1,42 @@
 <?php
 
-require '_classes.php';
-require '_database.php';
-require '_utilities.php';
 require '_configs.php';
+require '_league.php';
+require '_database.php';
 
 $db = database::connect();
-if ($db == null) 
+if ($db == null)
     exit();
 
-$all = get_all_leagues();
+$all = league::get_all_leagues();
 
 
-// reset all ranks in all leagues
-$db->query("UPDATE league SET rank=0");
-    
 // compute ranks for each league
 foreach ($all as $key => $item)
-    $db->multi_query("SET @r=0;UPDATE league SET rank=@r:=(@r+1) WHERE league_id=$item->id ORDER BY score DESC LIMIT 100000;");
+{
+    // reset all ranks
+    $db->query("UPDATE league_{$item->name} SET rank=0");
+
+    // set ranks base on scores
+    $db->multi_query("SET @r=0; UPDATE league_{$item->name} SET rank=@r:=(@r+1) WHERE score>{$item->base_score} ORDER BY score DESC LIMIT 100000;");
+}
 
 // update end for all leagues
 foreach ($all as $key => $item)
 {
     if ($item->is_ended())
-    {        
+    {
         // create list of 3 legends
         $rows = array();
-        $db->query("SELECT p.username, p.nickname, p.status, p.avatar, l.score, l.rank FROM league l LEFT JOIN profile p on l.profile_id=p.id WHERE l.league_id=$item->id && l.rank>=0 ORDER BY l.rank ASC LIMIT 3");
+        $db->query("SELECT p.username, p.nickname, p.status, p.avatar, l.score, l.rank FROM league_{$item->name} l LEFT JOIN profile p on l.profile_id=p.id WHERE l.score>{$item->base_score} && l.rank>0 ORDER BY l.rank ASC LIMIT 3");
         while($r = $db->result->fetch_assoc())
         {
             $rows[] = $r;
         }
         // save the list to a file as cache
-        file_put_contents(dirname(__FILE__) . '/cache/leaderboard_' . $item->id . '_last_3.txt', json_encode($rows), FILE_TEXT | LOCK_EX);
+        file_put_contents(dirname(__FILE__) . '/cache/leaderboard_' . $item->name . '_last_3.txt', json_encode($rows), FILE_TEXT | LOCK_EX);
 
-        $db->multi_query("UPDATE league SET end_score=score, end_rank=rank WHERE league_id=$item->id;UPDATE league SET rank=0, score=$item->base_score WHERE league_id=$item->id;");
+        $db->multi_query("UPDATE league_{$item->name} SET end_score=score, end_rank=rank;UPDATE league_{$item->name} SET rank=0, score={$item->base_score};");
     }
 }
 
--- games/carsparty/update-leagues-ranks.php
+++ games/carsparty/update-leagues-ranks.php
@@ -12,17 +12,18 @@ if ($db == null)
 $all = league::get_all_leagues();
 
 
-// compute positions for each league
+// compute ranks for each league
 foreach ($all as $key => $item)
 {
     // reset all ranks
     $db->query("UPDATE league_{$item->name} SET rank=0");
 
-    $db->multi_query("SET @r=0; UPDATE league_{$item->name} SET rank=@r:=(@r+1) ORDER BY score DESC LIMIT 100000;");
+    // set ranks base on scores
+    $db->multi_query("SET @r=0; UPDATE league_{$item->name} SET rank=@r:=(@r+1) WHERE score>{$item->base_score} ORDER BY score DESC LIMIT 100000;");
 
     // create list of 100 for each league
     $rows = array();
-    $db->query("SELECT p.username, p.nickname, p.status, p.avatar, l.score, l.rank FROM league_{$item->name} l LEFT JOIN profile p on l.profile_id=p.id WHERE l.rank>=0 ORDER BY l.rank ASC LIMIT 100");
+    $db->query("SELECT p.username, p.nickname, p.status, p.avatar, l.score, l.rank FROM league_{$item->name} l LEFT JOIN profile p on l.profile_id=p.id WHERE l.score>{$item->base_score} && l.rank>0 ORDER BY l.rank ASC LIMIT 100");
     while($r = $db->result->fetch_assoc())
     {
         $rows[] = $r;
